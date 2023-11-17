62e4408cf536cdfd4a0a80c633e17de3140093ee
auberamas <151127487+auberamas@users.noreply.github.com>
2023-11-17 09:22:50 +0100

    Add files via upload

---
+++ Pychatbot/functions.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  7 19:58:30 2023
+
+@author: Aube
+"""
+
+import os
+from math import *
+
+def list_of_files(directory, extension): # create a list of files name
+
+    files_names = []
+    for filename in os.listdir(directory): # scan the list of file in the directory
+        if filename.endswith(extension):# verify the extention of the file
+            files_names.append(filename)
+            
+    return (files_names)
+
+
+def name_of_presidents(files_name): # create a list of presidents name
+    
+    presidents_name = []
+    for name in files_name :
+        name = name.split('_') # create a list : [nomination, name.txt]
+        name[1] = name[1].strip('.txt')# clean the name from extension
+        
+        # clean the name from numbers
+        if name[1][-1].isdigit() :
+            name[1] = name[1].replace(name[1][-1],"")
+        
+        #add "'" 
+        if " " in name[1] :
+            sub_name = name[1].split(' ') # we work on the part after the space
+            for char in range(len(sub_name[1])-1) :
+               if sub_name[1][char].islower() and sub_name[1][char+1].isupper() :
+                    sub_name[1] = "'".join(sub_name[1][char:char+2]) + sub_name[1][char+2:]
+            name[1] = sub_name[0]+" " +sub_name[1]
+            
+        # verify if the name is already in the list of names          
+        if name[1] not in presidents_name : 
+            presidents_name.append(name[1])
+            
+    return(presidents_name)
+
+def fill_dico(name, first_name): # create a dico : {"first name" : "name"}
+    
+    Presidents = {}
+    for i in range(len(name)):
+        Presidents[name[i]] = first_name[i]
+        
+    return(Presidents)
+
+def display_full_name(dico_full_names): # display a dico as : key value
+    
+    for name in dico_full_names :
+        print(dico_full_names[name], name)
+
+# Create a list of lines of a file
+def list_lines_in_file(file) :
+    with open("cleaned2/"+ file, "r", encoding='utf-8') as f : 
+        lines = f.readlines()
+    return(lines)
+
+# Computation of Term Frequency
+def count_words(string): # create a dico : {"word" : occurence of the word }
+    
+    list_words = string.split(" ")
+    words_dico = dict()
+    for word in list_words :
+        if word not in words_dico and word != "" and word != "\n":
+            words_dico[word] = list_words.count(word)
+    return(words_dico)
+
+    
+# Calcul of the IDF (Inverse Document Frequency)  
+def dico_IDF(dico_tot, dico_files):
+    
+    IDF_score_dico = dict()
+    for word in dico_tot :
+        word_in_file, IDF = 0, 0
+        for file in dico_files :
+            if word in dico_files[file] :
+                word_in_file += 1 # To count the nb of file where is the word
+                
+        IDF = log(len(dico_files)/word_in_file)
+        IDF_score_dico[word] = IDF
+    return(IDF_score_dico)
+
+# Calculate the TF-IDF matrix
+def mat_TF_IDF(dico_tot, dico_files, IDF):
+    
+    mat = []    
+    for word in dico_tot :
+        line = []
+        for file in dico_files :
+            if word in dico_files[file] : 
+                line.append(dico_files[file][word] * IDF[word])
+            else :
+                line.append(0)
+        mat.append(line)   
+    return(mat)
+
+def less_important_words(dico_tot, mat):
+    
+    # Create a list with keys of dico_tot
+    dico_words = []
+    for key in dico_tot.keys():
+        dico_words.append(key) 
+    
+    # Create a list of less important words: with a TF-IDF vector equal to 0
+    nonimportant_words = []
+    for line in range(len(mat)) :
+        if sum(mat[line])== 0 :
+            nonimportant_words.append(dico_words[line])
+    
+    return(nonimportant_words)
+
+            
+            
+            
+    
+    
+
---
+++ Pychatbot/main.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  4 14:33:02 2023
+
+@author: Aube
+"""
+
+from functions import *
+    
+# Call of the function list_of_files
+directory = "./speeches"
+files_names = list_of_files(directory, "txt")
+
+if __name__ == '__main__':
+    # Call of the function name_of_presidents
+    presidents_name = name_of_presidents(files_names)
+    
+    # Create a dictionnary with name as key and first name as value
+    # Call the function fill_dico
+    presidents_first_name = ["Jacques", "Valéry", "François", "Emmanuel", "François", "Nicolas"]
+    dico_presidents = fill_dico(presidents_name, presidents_first_name)
+    
+    # Call the function display_full_name to diplay president name
+    display_full_name(dico_presidents)
+    print()
+#----DICTIONARY------------------------------------------------------------------------------
+    directory = "./cleaned2"
+    dictionary_of_files, total_dictionary = dict(), dict() 
+    string_of_file, total_string = str(), str()
+    
+    #Creation of a dico of words occurency for each file saved in dictionary_of_files
+    for file in files_names :
+        
+        with open(directory + "/" + file , "r", encoding = 'utf-8') as f:
+            for line in f :
+                for word in (line.strip()).split(' ') :
+                    string_of_file = string_of_file + " " + word
+        # Call the function count_words
+        dico_of_file = count_words(string_of_file)
+        # Fill the dico dictionary_of_files as : {"name file" : dico of the file }
+        dictionary_of_files[file] = dico_of_file
+        
+        total_string += string_of_file
+    
+    #Calcul of TF(term Frequency) : creation a dico of words occurency for all files 
+    # Call the function count_words
+    total_dictionary = count_words(total_string)
+#--------------------------------------------------------------------------------------------
+    
+    # Call the functions dico_IDF and mat_TF_IDF
+    IDF = dico_IDF(total_dictionary, dictionary_of_files)
+    mat = mat_TF_IDF(total_dictionary, dictionary_of_files, IDF)
+    
+    # Call the function less_important_words
+    list_less_imp = less_important_words(total_dictionary, mat)
+    print(list_less_imp)
+
+
